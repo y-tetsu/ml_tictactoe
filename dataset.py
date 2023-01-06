2ff912e983bdd0bab24c6281d3ed2d6ef55d8874
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-01-07 00:06:04 +0900

    add to_index

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import itertools
+import csv
 
 from tictactoe import B, O, X
 from scoring import RandomRecords
@@ -7,9 +8,24 @@ from scoring import RandomRecords
 class Patterns:
     def __init__(self, scores):
         self.scores = scores
-        pattern3 = enumerate(itertools.product([B, O, X], repeat=3))
-        self.to_index = {j: i for i, j in pattern3}
-        self.num = len(self.to_index.keys())
+        self.patterns = {
+            'h1': [0, 1, 2],
+            'h2': [3, 4, 5],
+            'h3': [6, 7, 8],
+            'v1': [0, 3, 6],
+            'v2': [1, 4, 7],
+            'v3': [2, 5, 8],
+            'd1': [0, 4, 8],
+            'd2': [2, 4, 6],
+        }
+        self.indexs = {}
+
+    def to_index(self, cells):
+        repeat = len(cells)
+        if repeat not in self.indexs:
+            product = enumerate(itertools.product([B, O, X], repeat=repeat))
+            self.indexs[repeat] = {j: i for i, j in product}
+        return self.indexs[repeat][cells]
 
 
 if __name__ == '__main__':
