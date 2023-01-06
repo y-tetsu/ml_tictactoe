392062aa6e122e9fa6bca3fd5205556ffd5b68a9
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-01-07 01:15:18 +0900

    add get_dataset

--- dataset.py
+++ dataset.py
@@ -9,14 +9,14 @@ class Patterns:
     def __init__(self, scores):
         self.scores = scores
         self.patterns = {
-            'h1': [0, 1, 2],
-            'h2': [3, 4, 5],
-            'h3': [6, 7, 8],
-            'v1': [0, 3, 6],
-            'v2': [1, 4, 7],
-            'v3': [2, 5, 8],
-            'd1': [0, 4, 8],
-            'd2': [2, 4, 6],
+            'h1': (0, 1, 2),
+            'h2': (3, 4, 5),
+            'h3': (6, 7, 8),
+            'v1': (0, 3, 6),
+            'v2': (1, 4, 7),
+            'v3': (2, 5, 8),
+            'd1': (0, 4, 8),
+            'd2': (2, 4, 6),
         }
         self.indexs = {}
 
@@ -33,9 +33,18 @@ class Patterns:
             header += [key + '-' + str(i) for i in range(3**len(value))]
         return header
 
+    def get_dataset(self, board, score):
+        dataset = {'rate': score['total'] / score['count']}
+        for key, value in self.patterns.items():
+            pattern = tuple([board[i] for i in value])
+            if key not in dataset:
+                dataset[key] = [0 for _ in range(3**len(value))]
+            dataset[key][self.to_index(pattern)] = 1
+        return dataset
+
 
 if __name__ == '__main__':
     scores = RandomRecords(10).scoring()
     patterns = Patterns(scores)
-    for i in patterns.get_header():
+    for i in patterns.get_dataset():
         print(i)
